1fb62796ad2a605cb64e459d882074bc03056c04
OnanefeOsah <57697426+OnanefeOsah@users.noreply.github.com>
2020-07-16 16:33:36 -0400

    Update BRTSYS.java
    Add functionality to load map

--- BRTSYS.java
+++ BRTSYS.java
@@ -1,10 +1,13 @@
-
+import java.util.Scanner;
 
 public class BRTSYS{
 
+    Scanner scan = new Scanner(System.in);
+    win_map m;
 
     BRTSYS(){
         menu();
+        choice(scan.nextInt());
     }
     
 
@@ -12,13 +15,30 @@ public class BRTSYS{
         System.out.println("Select a choice from the following options");
         System.out.println("1. View all buses for today");
         System.out.println("2. View map for bus-routes");
-        System.out.println("3. View city bustops");
+        System.out.println("3. View city bus-stops");
         System.out.println("4. View City of Windsor map");
         //System.out.println("5. Report");
     }
 
+    public void choice(int i){
+        switch(i){
+            case 1:
+                ;
+            case 2:
+                ;
+            case 3:
+                ;
+            case 4:
+                showMap();
+        }
+    }
+
     public void loadData(){
 
+    }
+
+    public void showMap(){
+        m = new win_map();
     }
 
     public static void main(String[] args){
